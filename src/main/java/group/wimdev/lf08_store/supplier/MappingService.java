0e0a9883ef8f85e06abb33a585eec4783c6e0a98
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-04 09:27:42 +0200

    FIX: Rename AddSupplierDTO to AddSupplierDto and add mapping method for SupplierEntity to GetSupplierDto

--- src/main/java/group/wimdev/lf08_store/supplier/MappingService.java
+++ src/main/java/group/wimdev/lf08_store/supplier/MappingService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MappingService {
-    public SupplierEntity mapAddSupplierDtoToSupplier(AddSupplierDTO dto) {
+    public SupplierEntity mapAddSupplierDtoToSupplier(AddSupplierDto dto) {
         SupplierEntity newSupplier = new SupplierEntity();
         newSupplier.setName(dto.getName());
         ContactEntity newContact = new ContactEntity();
@@ -14,6 +14,20 @@ public class MappingService {
         newContact.setCity(dto.getCity());
         newContact.setPhone(dto.getPhone());
         newContact.setSupplier(newSupplier);
+        newSupplier.setContact(newContact);
         return newSupplier;
+    }
+
+    public GetSupplierDto mapSupplierToGetSupplierDto(SupplierEntity supplier) {
+        GetSupplierDto dto = new GetSupplierDto();
+        dto.setId(supplier.getId());
+        dto.setName(supplier.getName());
+        if (supplier.getContact() != null) {
+            dto.setStreet(supplier.getContact().getStreet());
+            dto.setPostcode(supplier.getContact().getPostcode());
+            dto.setCity(supplier.getContact().getCity());
+            dto.setPhone(supplier.getContact().getPhone());
+        }
+        return dto;
     }
 }
