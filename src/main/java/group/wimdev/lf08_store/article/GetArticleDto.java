f4a1ee844655e12a31794c0129402b3bc56a0379
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-04 09:13:15 +0200

    Add GetArticleDto class for article retrieval data transfer

---
+++ src/main/java/group/wimdev/lf08_store/article/GetArticleDto.java
@@ -0,0 +1,10 @@
+package group.wimdev.lf08_store.article;
+
+import lombok.Data;
+
+@Data
+public class GetArticleDto {
+    private Long id;
+    private String designation;
+    private Double price;
+}
