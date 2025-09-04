4bcd8465dbe75a88e75a2a1024627da0af06090d
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-04 08:50:14 +0200

    Add SupplierRepository interface for SupplierEntity

---
+++ src/main/java/group/wimdev/lf08_store/supplier/SupplierRepository.java
@@ -0,0 +1,6 @@
+package group.wimdev.lf08_store.supplier;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierRepository extends JpaRepository<SupplierEntity, Long> {
+}
