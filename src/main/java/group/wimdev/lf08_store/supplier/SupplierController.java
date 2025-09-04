196c43472c3f2c88db68656c75840fc21f70436b
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-04 09:27:53 +0200

    Add SupplierController

---
+++ src/main/java/group/wimdev/lf08_store/supplier/SupplierController.java
@@ -0,0 +1,46 @@
+package group.wimdev.lf08_store.supplier;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.LinkedList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/store/supplier")
+public class SupplierController {
+    
+    private final SupplierService supplierService;
+    private final MappingService mappingService;
+    
+    public SupplierController(SupplierService supplierService, MappingService mappingService) {
+        this.supplierService = supplierService;
+        this.mappingService = mappingService;
+    }
+
+    @PostMapping
+    public ResponseEntity<GetSupplierDto> createSupplier(@Valid @RequestBody final AddSupplierDto dto) {
+        SupplierEntity newSupplier = this.mappingService.mapAddSupplierDtoToSupplier(dto);
+        newSupplier = this.supplierService.create(newSupplier);
+        final GetSupplierDto request = this.mappingService.mapSupplierToGetSupplierDto(newSupplier);
+        return new ResponseEntity<>(request, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GetSupplierDto>> findAllSuppliers() {
+        List<SupplierEntity> all = this.supplierService.readAll();
+        List<GetSupplierDto> dtoList = new LinkedList<>();
+        for (SupplierEntity supplier : all) {
+            dtoList.add(this.mappingService.mapSupplierToGetSupplierDto(supplier));
+        }
+        return new ResponseEntity<>(dtoList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GetSupplierDto> getSupplierById(@PathVariable final Long id) {
+        final var entity = this.supplierService.readById(id);
+        final GetSupplierDto dto = this.mappingService.mapSupplierToGetSupplierDto(entity);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+}
