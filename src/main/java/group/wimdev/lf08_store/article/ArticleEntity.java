5857cae85626403925cbb3f1e197a320ccb905de
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-04 08:41:04 +0200

    Add ArticleEntity class with validation constraints

---
+++ src/main/java/group/wimdev/lf08_store/article/ArticleEntity.java
@@ -0,0 +1,32 @@
+package group.wimdev.lf08_store.article;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "article")
+public class ArticleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Designation is mandatory")
+    private String designation;
+
+    @NotNull
+    private Double price;
+    @Column(name = "create_date", nullable = false)
+    private LocalDateTime createDate = LocalDateTime.now();
+
+    @Column(name = "last_update_date", nullable = false)
+    private LocalDateTime lastUpdateDate = LocalDateTime.now();
+}
