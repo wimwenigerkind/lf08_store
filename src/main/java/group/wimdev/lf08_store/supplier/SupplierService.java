0cbfc949fae7f0529fee0025ead932d07be9ca99
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-04 09:01:40 +0200

    Add SupplierService class to manage supplier operations

---
+++ src/main/java/group/wimdev/lf08_store/supplier/SupplierService.java
@@ -0,0 +1,15 @@
+package group.wimdev.lf08_store.supplier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SupplierService {
+    private final SupplierRepository supplierRepository;
+
+    public SupplierService(SupplierRepository supplierRepository) {
+        this.supplierRepository = supplierRepository;
+    }
+}
