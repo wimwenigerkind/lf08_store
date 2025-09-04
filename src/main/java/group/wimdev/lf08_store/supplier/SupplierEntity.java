0336922654e5591a892e17275e3736b6004fe84c
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-04 08:46:26 +0200

    Add SupplierEntity class with validation constraints

---
+++ src/main/java/group/wimdev/lf08_store/supplier/SupplierEntity.java
@@ -0,0 +1,25 @@
+package group.wimdev.lf08_store.supplier;
+
+import group.wimdev.lf08_store.contact.ContactEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "supplier")
+public class SupplierEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Size(max = 50, message = "Name must not exceed 50 characters")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private ContactEntity contact;
+}
