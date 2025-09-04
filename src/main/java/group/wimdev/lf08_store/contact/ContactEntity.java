68aae107eb002bfbc288e1bf37db6464941e3038
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-04 08:30:07 +0200

    Add ContactEntity class with validation constraints

---
+++ src/main/java/group/wimdev/lf08_store/contact/ContactEntity.java
@@ -0,0 +1,30 @@
+package group.wimdev.lf08_store.contact;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "supplier_contact")
+public class ContactEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Street is mandatory")
+    @Size(max = 50, message = "Street must not exceed 50 characters")
+    private String street;
+
+    @Column(name = "zip")
+    @NotBlank(message = "Postcode code is mandatory")
+    @Size(max = 5, message = "Postcode must have 5 characters")
+    private String postcode;
+
+    @NotBlank(message = "City is mandatory")
+    @Size(max = 50, message = "City must not exceed 50 characters")
+    private String city;
+
+    private String phone;
+}
