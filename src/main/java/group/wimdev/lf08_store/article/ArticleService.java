b1fdee71b6527a5863b961c60482002b460f1bbb
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-10 09:49:12 +0200

    Add ArticleService for managing article-related operations

---
+++ src/main/java/group/wimdev/lf08_store/article/ArticleService.java
@@ -0,0 +1,12 @@
+package group.wimdev.lf08_store.article;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+}
