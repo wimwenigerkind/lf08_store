2bad8ecd366deea01ffa23d9fc5378d3b0916f66
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-10 09:26:45 +0200

    Add ErrorDetails class for structured error response handling

---
+++ src/main/java/group/wimdev/lf08_store/exceptionhandling/ErrorDetails.java
@@ -0,0 +1,14 @@
+package group.wimdev.lf08_store.exceptionhandling;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class ErrorDetails {
+    private Date timestamp ;
+    private String message;
+    private String details;
+}
