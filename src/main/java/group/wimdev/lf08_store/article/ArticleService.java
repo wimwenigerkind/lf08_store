c9fbbd2d8f17fa232aaeb5bd2f0280d6f58c67bf
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-10 10:16:03 +0200

    Enhance ArticleService with supplier integration and new methods for article management

--- src/main/java/group/wimdev/lf08_store/article/ArticleService.java
+++ src/main/java/group/wimdev/lf08_store/article/ArticleService.java
@@ -1,17 +1,21 @@
 package group.wimdev.lf08_store.article;
 
 import group.wimdev.lf08_store.exceptionhandling.ResourceNotFoundException;
+import group.wimdev.lf08_store.supplier.SupplierService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ArticleService {
     private final ArticleRepository articleRepository;
+    private final SupplierService supplierService;
 
-    public ArticleService(ArticleRepository articleRepository) {
+    public ArticleService(ArticleRepository articleRepository, SupplierService supplierService) {
         this.articleRepository = articleRepository;
+        this.supplierService = supplierService;
     }
 
     public ArticleEntity create(ArticleEntity article) {
@@ -34,12 +38,28 @@ public class ArticleService {
         ArticleEntity updatedArticle = readById(article.getId());
         updatedArticle.setDesignation(article.getDesignation());
         updatedArticle.setPrice(article.getPrice());
-        updatedArticle.setPrice(article.getPrice());
         updatedArticle.setSupplier(article.getSupplier());
+        updatedArticle.setLastUpdateDate(LocalDateTime.now());
         return articleRepository.save(updatedArticle);
     }
 
     public void delete(Long id) {
         articleRepository.deleteById(id);
+    }
+
+    public ArticleEntity readByDesignation(String designation) {
+        Optional<ArticleEntity> article = articleRepository.findByDesignation(designation);
+        if (article.isEmpty()) {
+            throw new ResourceNotFoundException("Article with designation " + designation + " not found");
+        }
+        return article.get();
+    }
+
+    public ArticleEntity createForSupplier(ArticleEntity article, Long supplierId) {
+        var supplier = supplierService.readById(supplierId);
+        article.setSupplier(supplier);
+        article.setCreateDate(LocalDateTime.now());
+        article.setLastUpdateDate(LocalDateTime.now());
+        return articleRepository.save(article);
     }
 }
