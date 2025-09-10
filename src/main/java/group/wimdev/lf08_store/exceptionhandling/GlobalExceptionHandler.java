b7f1218448f1c48226473028e54b0b4c822b90d5
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-10 09:34:59 +0200

    Add exception handler for MethodArgumentNotValidException

--- src/main/java/group/wimdev/lf08_store/exceptionhandling/GlobalExceptionHandler.java
+++ src/main/java/group/wimdev/lf08_store/exceptionhandling/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package group.wimdev.lf08_store.exceptionhandling;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -31,5 +32,16 @@ public class GlobalExceptionHandler {
                 webRequest.getDescription(false)
         );
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetails> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false)
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
