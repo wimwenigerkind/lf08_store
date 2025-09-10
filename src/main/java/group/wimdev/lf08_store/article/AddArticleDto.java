14d4e06252f6e6a2d6c3b19415df411712ab50b1
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-10 10:00:28 +0200

    Add AddArticleDto for article creation with validation constraints

---
+++ src/main/java/group/wimdev/lf08_store/article/AddArticleDto.java
@@ -0,0 +1,21 @@
+package group.wimdev.lf08_store.article;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class AddArticleDto {
+    @NotBlank(message = "Designation is mandatory")
+    @Size(max = 50, message = "Designation must not exceed 50 characters")
+    private String designation;
+
+    @NotNull(message = "Price is mandatory")
+    @Positive(message = "Price must be positive")
+    private Double price;
+
+    @NotNull(message = "Supplier ID is mandatory")
+    private Long supplierId;
+}
