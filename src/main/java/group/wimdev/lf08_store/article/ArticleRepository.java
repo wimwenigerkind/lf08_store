2804642d54ecae10cc4fb2188bae7fa4719e33c9
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-09-10 10:16:12 +0200

    Add method to ArticleRepository for finding articles by designation

--- src/main/java/group/wimdev/lf08_store/article/ArticleRepository.java
+++ src/main/java/group/wimdev/lf08_store/article/ArticleRepository.java
@@ -2,5 +2,8 @@ package group.wimdev.lf08_store.article;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
+    Optional<ArticleEntity> findByDesignation(String designation);
 }
